7798fee630faa30597057000a91bc2ced6022f73
23mervinwijaya <23mervinwijaya@gmail.com>
2023-05-25 13:44:29 +0700

    mengubah file .ejs

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ app.get('/',(req,res)=>{
     res.render('index')
 })
 
+// About page
+app.get('/about',(req,res)=>{
+    res.render('about')
+})
+
+// Contact Page
+app.get('/contact',(req,res)=>{
+    res.render('contact')
+})
+
+//404 page
+app.use('/',(req,res)=>{
+    res.status(404);
+    res.send('<h1>404 : Page Not Found</h1>');
+})
+
 // Port Listen
 app.listen(port,()=>{
     console.log(`App is listening to port ${port}`)
